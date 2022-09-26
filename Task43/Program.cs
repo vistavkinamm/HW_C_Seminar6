4ed7e80a752498a1d8a3a92fb4e4d5a87bb22359
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-26 09:59:14 +0300

    task_43

---
+++ Task43/Program.cs
@@ -0,0 +1,21 @@
+﻿double pointX;
+double pointY;
+
+Console.Write("Введите значения b1, k1, b2 и k2 через пробел: ");
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int b1 = arr [1];
+int k1 = arr [3];
+int b2 = arr [0];
+int k2 = arr [2];
+
+if (b1 == k1)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else
+{
+    pointX = (double) (k2-b2)/(b1-k1);
+    pointY = (double) b1*(k2-b2)/(b1-k1) + b2;
+    Console.WriteLine("Координаты точки пересечения двух прямых:"+ $"Х {pointX:0.00}"+ $" Y {pointY:0.00}");
+    
+}
